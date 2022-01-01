27c0de1f80308aca7275e36ef102d2894c0c50b0
Dan Hanegbi <hanegbidan@gmail.com>
2022-01-01 22:07:50 +0200

    Added requirements.txt

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 import asyncio
+import subprocess
+import sys
 
 from consts import PRODUCE_IS_DONE
 from wiki_logger import WikiLogger
 from image_crawler import ImageCrawler
 from wikipedia_animals_api import WikipediaAnimalsApi
 
-if __name__ == "__main__":
+
+def run():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--no-cache-dir", "-r", "requirements.txt"])
+
     logger = WikiLogger().get_logger()
 
     wa = WikipediaAnimalsApi(logger=logger)
@@ -16,3 +21,7 @@ if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(ic.save_images(animals))
 
     logger.info(PRODUCE_IS_DONE)
+
+
+if __name__ == "__main__":
+    run()
